690f1afb897a9fbdeab9b5a3008126525bd91279
Piyush <piyushrajput846007@gmail.com>
2026-09-15 17:50:45 +0530

    Ingestion Pipeline completed

---
+++ backend/app/ingestion/test.py
@@ -0,0 +1,15 @@
+import asyncio
+from crawl4ai import AsyncWebCrawler
+
+
+async def main():
+    # Create an instance of AsyncWebCrawler
+    async with AsyncWebCrawler() as crawler:
+        # Run the crawler on a URL
+        result = await crawler.arun(url="https://leetcode.com/u/piyush__3007/")
+
+        # Print the extracted content
+        print(result.markdown)
+
+# Run the async main function
+asyncio.run(main())
